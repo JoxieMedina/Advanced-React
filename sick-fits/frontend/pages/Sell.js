f7bca942f0138925be56b71686bccbeb0fb64583
Darwin Medina <josuelp17@gmail.com>
2018-11-05 14:47:56 -0600

    configure eslint

---
+++ sick-fits/frontend/pages/Sell.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Link} from 'next'
+export default class Sell extends Component {
+  render() {
+    return (
+      <div>
+        Sell!
+        <Link>go to sell</Link>
+      </div>
+    )
+  }
+}
+
+
