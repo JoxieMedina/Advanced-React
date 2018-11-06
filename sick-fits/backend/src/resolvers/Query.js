cfa048d643a72f24f436facbf79922c2e28e4763
JoxieMedina <josuelp17@gmail.com>
2018-11-06 16:15:42 -0600

    setup prisma db, schemas and create item mutation

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    // Runs the same query that´s on the database
+  items: forwardTo('db'),
+  // async items(parent, args, ctx, info) {
+  //   console.log('Getting Items!!');
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
+};
 
 module.exports = Query;
